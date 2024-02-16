169f1f3b502b51cc3250a8d5e60c8c6b4ae65d8b
mudassirshah1335 <mudassirshar1335@gmail.com>
2024-02-16 07:54:17 -0800

    Login and Registered
    *I have created login and registered page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Login from "./Components/Auth-Components/Login";
+import Registration from "./Components/Auth-Components/Registration";
 function App() {
   return (
     <div className="bg-red">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Login />} />
+          <Route path="/sign-out" element={<Registration />} />
         </Routes>
       </BrowserRouter>
     </div>
