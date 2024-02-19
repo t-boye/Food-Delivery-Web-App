7ea234034174e8611aa19ba83ef32c490f6468e5
Emmanuel Tete Boye <emmanuelboye1957@gmail.com>
2024-02-20 02:16:56 +1400

    Revert "about Page"
    This reverts commit 9a636ad0853bebce5a3041160374c291657a1804.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,21 +1,14 @@
 import React from "react";
-import About_main from "../Components/About-Component/About_main";
-import Hero from "../Components/About-Component/Hero";
-import TeamCarousel from "../Components/About-Component/TeamCarousel";
-import AppPerfomance from "../Components/About-Component/AppPerfomance";
+import About_main from "../Components/About-Component/About_main"
+
 
 export default function About() {
   return (
-    <div className="bg-gray-200 ">
-      <Hero />
-      <About_main />
-      <div className="h-screen">
-        <h1 className="dancing-script  font-extrabold text-5xl py-12 text-center ">
-          Our Teams
-        </h1>
-        <TeamCarousel />
-      </div>
-      <AppPerfomance />
+    <div>
+   <About_main />
     </div>
   );
-}
+};
+
+
+
