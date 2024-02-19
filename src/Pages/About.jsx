9a636ad0853bebce5a3041160374c291657a1804
mudassirshah1335 <mudassirshar1335@gmail.com>
2024-02-19 03:46:49 -0800

    about Page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import About_main from "../Components/About-Component/About_main"
-
+import About_main from "../Components/About-Component/About_main";
+import Hero from "../Components/About-Component/Hero";
+import TeamCarousel from "../Components/About-Component/TeamCarousel";
+import AppPerfomance from "../Components/About-Component/AppPerfomance";
 
 export default function About() {
   return (
-    <div>
-   <About_main />
+    <div className="bg-gray-200 ">
+      <Hero />
+      <About_main />
+      <div className="h-screen">
+        <h1 className="dancing-script  font-extrabold text-5xl py-12 text-center ">
+          Our Teams
+        </h1>
+        <TeamCarousel />
+      </div>
+      <AppPerfomance />
     </div>
   );
-};
-
-
-
+}
