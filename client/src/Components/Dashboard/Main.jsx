5e4740475d44603a74193534e24dc327b5123789
mudassirshah1335 <mudassirshar1335@gmail.com>
2024-03-10 07:25:59 -0700

    dashboard

---
+++ client/src/Components/Dashboard/Main.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+
+const Main = () => {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+
+  return (
+    <>
+      <div className="grid-container">
+        <Header OpenSidebar={OpenSidebar} />
+        <Sidebar
+          openSidebarToggle={openSidebarToggle}
+          OpenSidebar={OpenSidebar}
+        />
+        <Home />
+      </div>
+    </>
+  );
+};
+
+export default Main;
